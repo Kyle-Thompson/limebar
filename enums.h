7a9b0c5afd710ecf12fbf4f4d63aa8c7e14e2d50
Kyle Thompson <kyle.thompson228@gmail.com>
2019-12-09 00:25:03 -0500

    removed unused enums

--- enums.h
+++ enums.h
@@ -1,17 +1,6 @@
 #pragma once
 
 enum {
-  ATTR_OVERL = (1<<0),
-  ATTR_UNDERL = (1<<1),
-};
-
-enum {
-  ALIGN_L = 0,
-  ALIGN_C,
-  ALIGN_R
-};
-
-enum {
   GC_DRAW = 0,
   GC_ACCENT,
   GC_CLEAR,
