8476430fe4a822b41c19f4b790935f0f60ef3f6e
Kyle Thompson <kyle.thompson228@gmail.com>
2020-03-29 19:48:03 -0400

    [CLEAN] removed logic from extract

--- modules/windows.cpp
+++ modules/windows.cpp
@@ -12,39 +12,37 @@
 #include "../x.h"
 
 
+// TODO it's probably an issue to have two different x connections. (conn, _x)
+
 mod_windows::mod_windows()
-    : conn(xcb_connect(nullptr, nullptr))
-    , current_desktop(get_atom(conn, "_NET_CURRENT_DESKTOP"))
-    , active_window(get_atom(conn, "_NET_ACTIVE_WINDOW"))
-    , x(X::Instance()) {
-  if (xcb_connection_has_error(conn)) {
+    : _conn(xcb_connect(nullptr, nullptr))
+    , _current_desktop(get_atom(_conn, "_NET_CURRENT_DESKTOP"))
+    , _active_window(get_atom(_conn, "_NET_ACTIVE_WINDOW"))
+    , _x(X::Instance()) {
+  if (xcb_connection_has_error(_conn)) {
     std::cerr << "Cannot X connection for workspaces daemon.\n";
     exit(EXIT_FAILURE);
   }
 
   uint32_t values = XCB_EVENT_MASK_PROPERTY_CHANGE;
-  xcb_screen_t *screen = xcb_setup_roots_iterator(xcb_get_setup(conn)).data;
-  xcb_change_window_attributes(conn, screen->root, XCB_CW_EVENT_MASK, &values);
-  xcb_flush(conn);
+  xcb_screen_t *screen = xcb_setup_roots_iterator(xcb_get_setup(_conn)).data;
+  xcb_change_window_attributes(_conn, screen->root, XCB_CW_EVENT_MASK, &values);
+  xcb_flush(_conn);
 }
 
 mod_windows::~mod_windows() {
-  xcb_disconnect(conn);
+  xcb_disconnect(_conn);
 }
 
 void
 mod_windows::extract(ModulePixmap *px) const {
-  // TODO: how to capture windows that don't work here? (e.g. steam)
-
-  for (Window w : _windows) {
-    auto workspace = x.get_property<uint32_t>(w, XA_CARDINAL, "_NET_WM_DESKTOP")
-                         .value()
-                         .at(0);
-    std::string title = x.get_window_title(w);
-    if (title.empty() || _current_workspace != workspace) continue;
+  for (const window_t& window : _windows) {
+    if (window.workspace != _current_workspace) {
+      continue;
+    }
 
     // TODO: don't add a space after the last window
-    px->write(title + " ", (w == _current_window));
+    px->write(window.title + " ", (window.id == _current_window));
   }
 }
 
@@ -52,25 +50,42 @@ void
 mod_windows::trigger() {
   while (true) {
     std::unique_ptr<xcb_generic_event_t, decltype(std::free) *> ev{
-        xcb_wait_for_event(conn), std::free};
+        xcb_wait_for_event(_conn), std::free};
     if (ev && (ev->response_type & 0x7F) == XCB_PROPERTY_NOTIFY) {
       auto atom =
           reinterpret_cast<xcb_property_notify_event_t *>(ev.get())->atom;
-      if (atom == active_window || atom == current_desktop) return;
+      if (atom == _active_window || atom == _current_desktop) {
+        return;
+      }
     }
   }
 }
 
 void
 mod_windows::update() {
-  _current_workspace = x.get_current_workspace();
-  _current_window = x.get_property<Window>(x.get_default_root_window(),
-                                           XA_WINDOW, "_NET_ACTIVE_WINDOW")
+  _current_workspace = _x.get_current_workspace();
+  _current_window = _x.get_property<Window>(_x.get_default_root_window(),
+                                            XA_WINDOW, "_NET_ACTIVE_WINDOW")
                         .value()
                         .at(0);
 
-  // TODO: must be a cleaner way to do this
-  auto temp = x.get_windows();
   _windows.clear();
-  _windows.insert(_windows.begin(), temp.begin(), temp.end());
+  for (auto windows = _x.get_windows(); auto window : windows) {
+    std::string title = _x.get_window_title(window);
+    if (title.empty()) {
+      continue;
+    }
+
+    auto workspace =
+        _x.get_property<uint32_t>(window, XA_CARDINAL, "_NET_WM_DESKTOP")
+            .value()
+            .at(0);
+
+    _windows.push_back({
+        .id = window,
+        .workspace = workspace,
+        .title = std::move(title)
+    });
+
+  }
 }
--- modules/windows.h
+++ modules/windows.h
@@ -3,12 +3,20 @@
 #include <X11/Xatom.h>
 #include <xcb/xcb.h>
 
+#include <string>
+
 #include "module.h"
 
 // TODO: make special window container
 
 class mod_windows : public DynamicModule<mod_windows> {
   friend class DynamicModule<mod_windows>;
+
+  struct window_t {
+    Window id;
+    xcb_atom_t workspace;
+    std::string title;
+  };
 
  public:
   mod_windows();
@@ -19,12 +27,12 @@ class mod_windows : public DynamicModule<mod_windows> {
   void trigger();
   void update();
 
-  xcb_connection_t* conn;
-  const xcb_atom_t current_desktop;
-  const xcb_atom_t active_window;
-  X& x;
+  xcb_connection_t* _conn;
+  const xcb_atom_t _current_desktop;
+  const xcb_atom_t _active_window;
+  X& _x;
 
   unsigned long _current_workspace { 0 };
   Window _current_window { 0 };
-  std::vector<Window> _windows;
+  std::vector<window_t> _windows;
 };
